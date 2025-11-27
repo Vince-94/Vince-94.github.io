7a3aea804c9bbac5773eb96f70e7b3e0392ec3a4
Vince-94 <57918895+Vince-94@users.noreply.github.com>
2025-11-27 19:32:59 +0000

    Deploying to gh-pages from @ Vince-94/Vince-94.github.io@e814d39b3b47618a4441a6d193592551ef874c66 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-repositories",
           title: "Repositories",
-          description: "",
+          description: "Here it is shown just the public contribution stats.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/repositories/";
